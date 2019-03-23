85a2297d788b71747bfc6f4f52d30c519790c5b9
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-23 12:07:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word (forward)
   array = forward.split(" ")
   array.each do |word|
-    reverse = word.reverse
-    reverse_string = reverse.push
+    reverse_word = word.reverse
+    reverse_phrase = []
+    reverse_phrase.push(reverse_word)
   end
   reverse_string
 end
